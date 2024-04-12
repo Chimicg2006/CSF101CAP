b03922acd519f8e579088b0adb2020d0fe276c49
unknown <02230060.cst@rub.edu.bt>
2024-04-12 20:33:40 +0600

    changes to CAP_02230060.py

--- CAP_02230060.py
+++ CAP_02230060.py
@@ -12,35 +12,16 @@
 # Your Solution Score: 50054
 # Put your number here: 0
 ################################
-# Read the input.txt file
 def read_input():
-    f = open('input_0_cap1.txt', 'r')
-    print(f.read())
-    f.close()
+    return open('CSF101CAP/input_0_cap1.txt','r')
 
-# code here to read your input file
-# solution
-def calculate_score():
-    read=open('input_0_cap1.txt', 'r')
-    reader=read.read()
-    score=0
-    for i in reader: 
-        if i=="A":
-            score+=1 # 1 for rock
-        elif i== "B":
-            score+=2 # 2 for paper
-        elif i=="C":
-            score+=3 # 3 for scissor
-        elif i=="X":
-            score+=0 # 0 for lossing the round
-        elif i=="Y":
-            score+=3 # 3 for end have having draw
-        elif i=="Z":
-            score+=6 # 6 for winning the round
-    print("score is",score)
-calculate_score()
+def calculate_score(textfile):
+    store= {'A X': 2, 'A Y': 4, 'A Z': 9, 'B X': 1, 'B Y': 5, 'B Z': 7, 'C X': 1, 'C Y': 6, 'C Z': 7} 
+    score = 0
+    for text in textfile:
+        value = text.strip()
+        if value in store:
+            score += store[value]
+    print("total value: ",score)
 
-# implement your solution here.
-# print your solution to output as:"the total score is: <score>"
-# Other parts of code here to run your functions and printing of the input.
-
+calculate_score(read_input())
